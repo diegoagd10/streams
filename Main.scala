20f8de573c65f78f2bb3d5ec00be37fabeabebe4
Diego Antonio <diegogomez211990@gmail.com>
2019-01-14 18:38:48 -0800

    Initial commit

---
+++ Main.scala
@@ -0,0 +1,18 @@
+package org.dgomezdiaz.streams
+
+import scala.io.StdIn._
+
+object Main extends App {
+  def multiple(numero: Int, contador: Int = 0): Unit = {
+      if(contador > 1000) ()
+      else {
+         if(contador % numero == 0) println(s"El $contador es mültiplo de $numero, todo cool")
+         else                       System.err.println(s"NO!!! ALARMA: el $contador no es mültiplo de $numero!!!")
+         Thread.sleep(1000)
+         multiple(numero, contador + 1)
+      }
+  }
+
+  println("nümero: ")
+  multiple(readInt())
+}
---
+++ build.sbt
@@ -0,0 +1,4 @@
+version		:= "0.0.0-SNAPSHOT"
+organization	:= "org.dgomezdiaz"
+name		:= "streams"
+scalaVersion	:= "2.12.8"
